4733cb8c97ec76a7ddef326a375419d029aa6874
Ernesto Casablanca <casablancaernesto@gmail.com>
2022-02-21 20:01:21 +0100

    feat(backend): add utility scripts to test the api

---
+++ backend/scripts/api.js
@@ -0,0 +1,23 @@
+const fetch = require("node-fetch");
+
+const delays = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120];
+const url = "http://localhost:3000";
+const asset_id = "0x8d12a197cb00d4747a1fe03395095ce2a5cc6819";
+delays.forEach(async (delay) => {
+  const res = await fetch(url + "/" + asset_id, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify({
+      readings: [
+        {
+          timestamp: new Date(Date.now() + delay * 1000).toISOString(),
+          value: Math.floor(Math.random() * 10000),
+        },
+      ],
+      unit: "kWh",
+    }),
+  });
+  console.log(await res.text());
+});
