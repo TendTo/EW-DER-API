472bd3ef6bb3321a5e21d887faad0c126afb1928
Ernesto Casablanca <casablancaernesto@gmail.com>
2022-04-06 00:15:26 +0200

    fix(der): der script

--- der/iot_source.sh
+++ der/iot_source.sh
@@ -3,7 +3,7 @@ while :
 do
     timestamp=$(date -u +%Y-%m-%dT%H:%M:%S.000Z)
     value=$(shuf -i 100-10000 -n 1)
-    if [ "$NEGATIVE_CONSUME" = "true" ]; then value=$((-value)); fi
+    if [ "${NEGATIVE_CONSUME:-false}" = "true" ]; then value="-$value"; fi
     json="
     {
         \"assetDID\": \"${ASSET_DID:-missing-ASSET_DID}\",
@@ -12,5 +12,5 @@ do
         \"unit\": \"Wh\"
     }"
     curl -X POST -H "Content-Type: application/json" -d "$json" "${PROSUMER_URL:-missing-PROSUMER_URL}/readings"
-    sleep ${SLEEP_SEC:-5}s
+    sleep "${SLEEP_SEC:-5}s"
 done
