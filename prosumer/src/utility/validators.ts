9c12aa231d72d8303a2038805ad27fd77d4eb04c
TendTo <casablancaernesto@gmail.com>
2022-04-18 21:47:27 +0200

    fix(prosumer): fix DID regex

--- prosumer/src/utility/validators.ts
+++ prosumer/src/utility/validators.ts
@@ -1,7 +1,7 @@
 import { Matches } from "class-validator";
 
 export function IsDID() {
-  return Matches(/^(did:ethr:)?(0x)?[0-9a-f]{40}$/, {
+  return Matches(/^did:ethr:0x[0-9a-fA-F]{40}$/, {
     message:
       "Invalid format. Expected a DID. Check https://github.com/uport-project/ethr-did#did-method",
   });
