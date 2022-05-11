6cf1873bc52b81207c65ff0ab63ac87deac031b4
Ernesto Casablanca <casablancaernesto@gmail.com>
2022-05-11 18:37:32 +0200

    chore(contracts): typo in comment

--- contract/contracts/MeteredReadings/ReadingsNotary.sol
+++ contract/contracts/MeteredReadings/ReadingsNotary.sol
@@ -6,7 +6,7 @@ contract ReadingsNotary {
     event NewMeterReading(address indexed operator, bytes indexed proof);
 
     /** Store a new reding
-     * @param _proof Merkel root of the tree of merkle proofs of the aggregated readings
+     * @param _proof Merkle root of the tree of merkle proofs of the aggregated readings
      */
     function store(bytes calldata _proof) external {
         emit NewMeterReading(msg.sender, _proof);
