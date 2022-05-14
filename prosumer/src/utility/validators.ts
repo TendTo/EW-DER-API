7507ac31591d6037256d61bc35cf7cbb34cf72cb
TendTo <casablancaernesto@gmail.com>
2022-05-14 18:14:47 +0200

    fix(prosumer): update validator to match the aggregator's one

--- prosumer/src/utility/validators.ts
+++ prosumer/src/utility/validators.ts
@@ -1,8 +1,9 @@
-import { Matches } from "class-validator";
+import { Matches, ValidationOptions } from "class-validator";
 
-export function IsDID() {
+export function IsDID(validationOptions?: ValidationOptions) {
   return Matches(/^did:ethr:0x[0-9a-fA-F]{40}$/, {
     message:
       "Invalid format. Expected a DID. Check https://github.com/uport-project/ethr-did#did-method",
+    ...validationOptions,
   });
 }
